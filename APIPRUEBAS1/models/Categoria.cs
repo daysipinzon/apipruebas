2f139e3a06fcd18ad5bdc5b3c374065cad73eb94
daysipinzon022 <daysipinzon033@outlook.com>
2024-09-12 14:15:44 -0600

    Agregar archivos de proyecto.

---
+++ APIPRUEBAS1/models/Categoria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIPRUEBAS1.Models;
+
+[Table("CATEGORIA")]
+public partial class Categoria
+{
+    [Key]
+    public int IdCategoria { get; set; }
+
+    [StringLength(50)]
+    [Unicode(false)]
+    public string? Descripcion { get; set; }
+
+    
+    public virtual ICollection<Producto> Producto { get; set; } = new List<Producto>();
+}
---
+++ APIPRUEBAS1/models/Producto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIPRUEBAS1.Models;
+
+[Table("PRODUCTO")]
+public partial class Producto
+{
+    [Key]
+    public int IdProducto { get; set; }
+
+    [StringLength(20)]
+    [Unicode(false)]
+    public string? CodigoBarra { get; set; }
+
+    [StringLength(50)]
+    [Unicode(false)]
+    public string? Descripcion { get; set; }
+
+    [StringLength(50)]
+    [Unicode(false)]
+    public string? Marca { get; set; }
+
+    public int? IdCategoria { get; set; }
+
+    [Column(TypeName = "decimal(10, 2)")]
+    public decimal? Precio { get; set; }
+
+    [ForeignKey("IdCategoria")]
+    [JsonIgnore]
+    public virtual Categoria? oCategoria { get; set; }
+}
